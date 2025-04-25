e807369c7260eb18f525826a7a230e334e14b902
CyberTycoon <silasokanla2006@gmail.com>
2025-04-25 20:50:26 +0100

    mobile navbar fix

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -96,7 +96,7 @@ const Navbar = () => {
                 >
                   LogOut
                 </button>
-                {dashboard == false && (
+                {(dashboard == false) && (
                   <Link
                     href="/dashboard"
                     className="bg-gradient-to-r from-amber-500 to-orange-600 text-gray-900 font-semibold px-4 py-2 rounded-lg hover:from-amber-400 hover:to-orange-500 transition-all duration-300 shadow-md shadow-amber-900/20"
@@ -170,16 +170,36 @@ const Navbar = () => {
             </a>
             <Link
               href="/login"
-              className="text-amber-100/80 hover:text-amber-400 transition-colors w-full"
+              prefetch
+              className={isLoggedIn?'hidden' : "text-amber-100/80 hover:text-amber-400 transition-colors w-full"}
             >
               Login
             </Link>
             <Link
+              prefetch
               href="/signup"
-              className="bg-gradient-to-r from-amber-500 to-orange-600 text-gray-900 font-semibold px-4 py-2 rounded-lg hover:from-amber-400 hover:to-orange-500 transition-all duration-300 shadow-md shadow-amber-900/20 w-full text-center"
+              className={isLoggedIn ? 'hidden' : "bg-gradient-to-r from-amber-500 to-orange-600 text-gray-900 font-semibold px-4 py-2 rounded-lg hover:from-amber-400 hover:to-orange-500 transition-all duration-300 shadow-md shadow-amber-900/20 w-full text-center"}
             >
               Sign Up
             </Link>
+            {isLoggedIn && (
+              <>
+                <button
+                  onClick={handleLogout}
+                  className="bg-gradient-to-r from-amber-500 to-orange-600 text-gray-900 font-semibold px-4 py-2 rounded-lg hover:from-amber-400 hover:to-orange-500 transition-all duration-300 shadow-md shadow-amber-900/20 w-full text-center"
+                >
+                  LogOut
+                </button>
+                {dashboard == false && (
+                  <Link
+                    href="/dashboard"
+                    className="bg-gradient-to-r from-amber-500 to-orange-600 text-gray-900 font-semibold px-4 py-2 rounded-lg hover:from-amber-400 hover:to-orange-500 transition-all duration-300 shadow-md shadow-amber-900/20 w-full text-center"
+                  >
+                    Dashboard
+                  </Link>
+                )}
+              </>
+            )}
           </div>
         )}
       </div>
